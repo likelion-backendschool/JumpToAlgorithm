13900880d1a8c1dd3f6679c28a3c313ac48e704c
garamminchan <garamminchan@gmail.com>
2022-07-14 16:34:05 +0900

    conflict 2

--- src/chanki/Main.java
+++ src/chanki/Main.java
@@ -2,7 +2,7 @@ package chanki;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("임시 브랜치");
-        System.out.println("추가 커밋");
+        System.out.println("임시 브랜치zzzzzzzzzz");
+        System.out.println("추가 커밋zzzzzzzzzzzzzz");
     }
 }
