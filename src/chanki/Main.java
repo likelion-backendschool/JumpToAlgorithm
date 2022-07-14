33a3f2e125c740d210d0334e6513d8545dbf58f2
garamminchan <garamminchan@gmail.com>
2022-07-14 16:33:30 +0900

    conflict

--- src/chanki/Main.java
+++ src/chanki/Main.java
@@ -2,7 +2,7 @@ package chanki;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("임시 브랜치");
-        System.out.println("추가 커밋");
+        System.out.println("임시 브랜치 컨플릭트");
+        System.out.println("추가");
     }
 }
