7bc951cfb1d94af8d246f69369396568fdb96f98
somang-lim <somangjuha@gmail.com>
2022-07-16 00:31:13 +0900

    [Baekjoon-1309]피보나치 수 2

---
+++ src/somang/week3/Q2748.java
@@ -0,0 +1,31 @@
+package somang.week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2748 {
+
+    /*
+        [Baekjoon-1309]피보나치 수 2
+        ⏰ 80ms
+     */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N + 2];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[N]);
+    }
+
+}
