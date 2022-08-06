de818f59a50afd5769404c3d3127b9048926a5a1
Shin-sangwon <dnjsml10@gmail.com>
2022-08-06 23:04:31 +0900

    [Baekjoon-2164]카드2

---
+++ src/sangwon/week5/Q2164.java
@@ -0,0 +1,31 @@
+package sangwon.week5;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//week5 Q2162 카드2
+public class Q2164 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 1; i <= n; i++){
+            q.add(i);
+        }
+
+        while(q.size() != 1){
+            q.poll();
+            q.add(q.poll());
+        }
+
+        bw.write(q.peek()+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
