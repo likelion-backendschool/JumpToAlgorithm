6cdb32523f6d433964a083f8230039b45e7637a8
devmizz <garamminchan@gmail.com>
2022-08-14 21:55:42 +0900

    [Baekjoon-14425]문자열 집합

---
+++ src/chanki/week5/BJ14425.java
@@ -0,0 +1,35 @@
+package chanki.week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BJ14425 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputs = br.readLine().split(" ");
+
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+
+        ArrayList<String> set = new ArrayList<>();
+        String input;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++) {
+            input = br.readLine();
+            set.add(input);
+        }
+
+        for(int i = 0; i < m; i++) {
+            input = br.readLine();
+            if(set.contains(input)) {
+                sum++;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
