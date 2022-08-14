6196755abd4eb803c963816529d24273b0e2f5a8
Shin-sangwon <dnjsml10@gmail.com>
2022-08-14 21:49:32 +0900

    [Baekjoon-14425]문자열 집합

---
+++ src/sangwon/week6/BJ14425.java
@@ -0,0 +1,40 @@
+package sangwon.week6;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class BJ14425 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashMap<String, Boolean> map = new HashMap<>();
+        int ans = 0;
+
+        while(n--> 0) {
+            map.put(br.readLine(), true);
+        }
+
+        while(m--> 0) {
+            if(map.get(br.readLine()) != null) ans += 1;
+        }
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+        br.close();
+
+
+    }
+}
